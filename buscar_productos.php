efd9925b19cca497af63c67b0f3338db449ad1df
wost384-ops <wost384@gmaol.com>
2025-11-23 22:39:09 -0500

    Actualización del proyecto

--- buscar_productos.php
+++ buscar_productos.php
@@ -4,10 +4,12 @@ require 'includes/db.php';
 $q = $_GET['q'] ?? '';
 
 if($q){
-    $stmt = $pdo->prepare("SELECT id, nombre, precio FROM productos WHERE nombre LIKE ? LIMIT 10");
+    $stmt = $pdo->prepare("SELECT id, nombre, precio FROM productos WHERE nombre LIKE ? LIMIT 10"); // SKU ELIMINADO
     $stmt->execute(["%$q%"]);
     $productos = $stmt->fetchAll(PDO::FETCH_ASSOC);
     echo json_encode($productos);
 } else {
     echo json_encode([]);
 }
+
+?>
